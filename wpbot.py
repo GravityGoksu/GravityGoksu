0ba7d1536dec4d0294242aa287e81359498a4236
GravityGoksu <69727335+GravityGoksu@users.noreply.github.com>
2021-12-28 14:10:27 +0300

    Create wpbot.py

---
+++ wpbot.py
@@ -0,0 +1,281 @@
+import pywhatkit as w
+import time
+import pyautogui
+import schedule
+import keyboard as k
+import pyautogui, sys
+
+
+with open("sozcukler.txt", "r", encoding='utf-8') as f:
+    for line in f:
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "hardware : donanım , buffer : arabellek", 5, 48)
+        pyautogui.click(1571, 987)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "action bar : menü çubuğu , software : yazılım", 4, 30)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "computer : bilgisayar , algorithm : algoritma", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "attachment : bağlantı , engineer : mühendis", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "computer vırus : bilgisayar virüsü , auxiliary storage : yardımcı bellek", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "bulk storage : yığın bellek , cache : önbellek", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "server : sunucu , code : kod", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "loop : döngü , mouse : fare", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "book : kitap , desktop : masaüstü", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "input : girdi , coding : kodlama", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "output: çıktı , interaction : etkileşim", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "interpreter : yorumlayıcı , mainboard : anakart", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "preprocessor : önişlemci , print : yazdırmak", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "process : süreç , programming : programlama", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "browser : tarayıcı , network : ağ", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "cyber attack : siber saldırı , homepage : anasayfa", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "interface : arayüz , update : güncelleme", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "computer babble : bilgisayar paraziti , character: karakter", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "error : hata , data mining : veri madenciliği", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "decoding : kod çözme , system : sistem", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "virtual address : sanal adres , computer worms : bilgisayar solucanı", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "dısk driver : disk sürücüsü , cyber attack : siber saldırı", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "access : erişim , feedback , geri bildirim", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "wireless access : kablosuz erişim , housing : bilgisayar kasası", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "folder : klasör , artificial intelligence : yapay zeka", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "data warehouse  : vei ambarı , programmer : programcı", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "code analysis : kod analizi , operator : operatör", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "database : veritabanı, data : veri", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "decoding : kod çözme , system :sistem", 11, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+        pyautogui.click(1896, 10)
+        time.sleep(20)
+        k.press_and_release('enter')
+
+        w.sendwhatmsg_to_group('+90 532 588 22 38', "interpreter : yorumlayıcı , mainboard : anakart", 15, 00)
+        pyautogui.click(1295, 600)
+        time.sleep(2)
+        k.press_and_release('enter')
+
+    stop()
+
+
+
+
+
+
+
+
+
+
+#d1 = open("sozcukler.txt","r")
+#d1_veri = d1.read()
+#d1_veriler = d1_veri.split('/n')
+#for i in range(len(d1_veriler)):
+#  if(d1_veriler[i]!=""):
+#    print(d1_veriler[i])
+
+#
+#    w.sendwhatmsg('+90 532 588 22 38', d1_veriler[i], 2, 16)
+#    pyautogui.click(1295, 600)
+#    time.sleep(2)
+#    k.press_and_release('enter')
+#    print(d1_veriler[i])
+#
+#d1.close()
+
+
+
+
+
+
+
